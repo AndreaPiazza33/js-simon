4236c3454e07fb581a079c92453e6f9a7fe8a14c
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-08-23 17:01:29 +0200

    feat: add time (tomorrow)

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ console.log(today);
 let timeNow =parseInt(today.getTime());
 console.log(timeNow);
 
+//tempo a domani (09.30)
+const tomorrow = new Date(2023,7,24,9,30);
+console.log(tomorrow);
+let timeTomorrow =parseInt(tomorrow.getTime());
+console.log(timeTomorrow);
+
+
