75d7e17438e95c91fe7d00eb5b69af191b893f16
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-08-24 14:48:45 +0200

    feat: add interval

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
-// tempo ad oggi
-const today = new Date();
-console.log(today);
-let timeNow =parseInt(today.getTime());
-console.log(timeNow);
 
-//tempo a domani (09.30)
-const tomorrow = new Date(2023,7,24,9,30);
-console.log(tomorrow);
-let timeTomorrow =parseInt(tomorrow.getTime());
-console.log(timeTomorrow);
+setInterval(timer, 1000)
+function timer (){
+    // tempo ad oggi
+    const today = new Date();
+    let timeNow =parseInt(today.getTime());
 
-//tempo rimanente (in secondi)
-let secTimeRemaining = Math.floor((timeTomorrow - timeNow) / 1000)
-//tempo rimanente (in minuti)
-let minTimeRemaining = Math.floor(secTimeRemaining / 60)
-//tempo rimanente (in ore)
-let hourTimeRemaining = Math.floor(minTimeRemaining / 60)
-//tempo rimanente (in giorni)
-let dayTimeRemaining = Math.floor(hourTimeRemaining / 24)
-console.log('secondi : ',secTimeRemaining);
-console.log('minuti : ',minTimeRemaining);
-console.log('ore : ',hourTimeRemaining);
-console.log('giorni : ',dayTimeRemaining);
+    //tempo a domani (09.30)
+    const tomorrow = new Date(2023,7,24,9,30);
+    let timeTomorrow =parseInt(tomorrow.getTime());
+
+    let timeRemaining = (timeTomorrow - timeNow)    
+    //tempo rimanente (in giorni)
+    let dayTimeRemaining = Math.floor(timeRemaining / (1000 * 60 * 60 * 24))
+    document.getElementById('days').innerText = dayTimeRemaining
+   //tempo rimanente (in ore)
+    let hourTimeRemaining = Math.floor(timeRemaining % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    document.getElementById('hours').innerText = hourTimeRemaining
+    //tempo rimanente (in minuti)
+    let minTimeRemaining = Math.floor(timeRemaining % (1000 * 60 * 60 ) / (1000 * 60))
+    document.getElementById('minutes').innerText = minTimeRemaining
+    //tempo rimanente (in secondi)
+    let secTimeRemaining = Math.floor(timeRemaining % (1000 * 60) / 1000)
+    document.getElementById('seconds').innerText = secTimeRemaining
+        if (timeRemaining < 0) {
+        clearInterval(timer)
+    } 
+}
+
+
+
